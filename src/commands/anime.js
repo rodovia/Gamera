af4fd54742376633d890c7475055fdd264d17118
João Henrique <joaoenrique.san@gmail.com>
2021-01-28 15:43:40 -0300

    Terminar implementação do comando de anime 0.1

--- src/commands/anime.js
+++ src/commands/anime.js
@@ -3,22 +3,30 @@ const scraper = require('mal-scraper');
 
 const search = scraper.search.search
 
-
 const searchOptions = {   //Opções de pesquisa
-    maxResults : 10 
+    maxResults:5
 };
 
 
 async function execute(client, message, args){
+    let resposta;
     let anime = message.content.slice(7); // Tira a parte "*anime" do comando e deixa apenas o nome do anime
     searchOptions.term = anime; //Termo a ser procurado
-    let resultados = await search('anime', searchOptions); //Procura o anime e retorna um json
-    let listaDeAnimes = [];
-    for(id = 0; id < resultados.length ; id++){
-        listaDeAnimes.push(resultados[id].title);
+
+    let info = await scraper.getInfoFromName(anime);
+    //Verifica se existe algum anime com o nome digitado
+    if(anime.toLowerCase() == info.title.toLowerCase()){  
+        resposta = `${info.picture}\nanime: ${info.title} \nsinopse: ${info.synopsis}`
     }
-    console.log(listaDeAnimes);
-    message.reply(listaDeAnimes);
+    //Caso não haja retorna uma lista de animes com nomes parecidos
+    else{let resultados = await search('anime', searchOptions); //Procura o anime e retorna um json
+        let listaDeAnimes = [];
+        for(id = 0; id < searchOptions.maxResults ; id++){
+            listaDeAnimes.push(resultados[id].title);
+        }
+        resposta = `\nPossíveis animes: \n${listaDeAnimes}`
+    }
+    message.reply(resposta)
     
 }
 
