10d04dee56339ce230399554ba9217fc8cc30d45
rodovia <64744799+rodovia@users.noreply.github.com>
2021-01-28 21:41:31 -0300

    Update ping.js

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -1,14 +1,8 @@
 const discord = require("discord.js");
 
-<<<<<<< HEAD
 async function execute(client, message, args) {
-    let lat = Date.now() - message.createdTimestamp
-    message.reply(`Pong! ${lat}ms https://tenor.com/KWO8.gif`);
-=======
-function execute(client, message, args) {
     let lat = Math.abs(Date.now() - message.createdTimestamp);
     message.reply(`Pong! ${lat}ms https://giphy.com/gifs/tennis-4IAzyrhy9rkis`);
->>>>>>> 171144f1801d714c22774b37e7968ae3f59f830a
 }
 
 module.exports = {
